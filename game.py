81ad2a838718ccc2e0ad9e4bc1cc1343a6509fd6
annaorr <annaworr@gmail.com>
2026-09-11 23:23:42 -0500

    Update game.py

--- game.py
+++ game.py
@@ -18,9 +18,9 @@ def main():
 
     goblinTwo = Goblin("Scribble")
     
-        print(f"{goblin.name} enters the arena with {goblin.health} health.")
-        print("But no hero has answered the call... yet.")
-    
+    print(f"{goblinTwo.name} enters the arena with {goblinTwo.health} health.")
+    print("But no hero has answered the call... yet.")
+
 
 
 if __name__ == "__main__":
