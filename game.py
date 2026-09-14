343d982664b320df955f6e6588e5c4dca38d4506
annaorr <annaworr@gmail.com>
2026-09-14 15:29:07 -0500

    hey guys i added a hero shaboya

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 from goblin import Goblin
-
+from hero import Hero
 
 ARENA_NAME = "The Cirle Arena"
 
@@ -9,19 +9,27 @@ def main():
     print(f"Welcome to {ARENA_NAME}!")
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
     print("The gates are opening...")
+    
 
     goblin = Goblin("Gubble")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
-    
-
 
     goblinTwo = Goblin("Scribble")
+
+    bianca = Hero("bianca")
+    bianca.AttackNumber = bianca.attack()
+    print("Bianca screams: GOTCHA")
+    goblin.take_damage(bianca.AttackNumber)
+    if goblin.health > 0:
+        goblinAttackNumber = goblin.attack()
+        print("WATCH OUT BIANCA IT'S COMING FOR YOU")
+        bianca.take_damage(goblinAttackNumber)
     
-    print(f"{goblinTwo.name} enters the arena with {goblinTwo.health} health.")
-    print("But no hero has answered the call... yet.")
 
 
+    aragorn = Hero("Aragorn")
+    heroAttack = aragorn.attack()
 
 if __name__ == "__main__":
     main()
